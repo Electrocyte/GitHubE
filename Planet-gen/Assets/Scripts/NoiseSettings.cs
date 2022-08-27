fceb6178a5b500d2ab1f31db53793c1bb04492fb
James <10740291+Electrocyte@users.noreply.github.com>
2022-08-28 06:54:54 +0800

    ridge test

--- Planet-gen/Assets/Scripts/NoiseSettings.cs
+++ Planet-gen/Assets/Scripts/NoiseSettings.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 [System.Serializable]
 public class NoiseSettings 
 {
-    public float strength = 1f;
-    public float rougness = 2f;
-    public float BaseRougness = 1f;
-    public float persistence = 0.5f;
-    [Range(1,8)]
-    public int numLayers = 1;
-    public Vector3 centre;
-    public float minValue;
+    public enum FilterType {Simple, Rigid};
+    public FilterType filterType;
+
+    // [ConditionalHide("filterType", 0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+    // [ConditionalHide("filterType", 1)]
+    public RigidNoiseSettings rigidNoiseSettings;
+
+    [System.Serializable]
+    public class SimpleNoiseSettings {
+        public float strength = 1f;
+        public float rougness = 2f;
+        public float BaseRougness = 1f;
+        public float persistence = 0.5f;
+        [Range(1,8)]
+        public int numLayers = 1;
+        public Vector3 centre;
+        public float minValue;
+    }
+
+    [System.Serializable]
+    public class RigidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = .8f;
+    }
 }
