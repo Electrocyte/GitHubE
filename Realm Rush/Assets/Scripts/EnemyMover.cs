eb21ed2372ac66befa158e8f99d1237ba0770f75
James <10740291+Electrocyte@users.noreply.github.com>
2022-08-31 09:45:21 +0800

    fuse and movement for cake of death

--- Realm Rush/Assets/Scripts/EnemyMover.cs
+++ Realm Rush/Assets/Scripts/EnemyMover.cs
@@ -7,7 +7,8 @@ public class EnemyMover : MonoBehaviour
 {
     
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] GameObject fuse;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +18,37 @@ public class EnemyMover : MonoBehaviour
 
     private IEnumerator FollowPath()
     {
+        int n = 0;
         foreach (var wp in path)
         {
-            transform.position = wp.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = wp.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            var currentGameObj = gameObject.tag;
+            n++;
+            while (travelPercent < 1f) {
+                travelPercent += Time.deltaTime * speed;
+
+                if (currentGameObj == "Cake") {   
+
+                    if (path.Count == n) {
+                        Debug.Log("Fuse runs out.");
+                    }
+
+                    if (Time.frameCount > 60f / speed) {
+                        endPosition.y = 3;
+                        transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                    }
+
+                } else {
+                    transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                }
+                yield return new WaitForEndOfFrame();
+            }
+
         }
     }
 }
