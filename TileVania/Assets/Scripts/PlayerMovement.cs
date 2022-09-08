5d8d87e3559b9698411f6e50727555cccf26171f
James <10740291+Electrocyte@users.noreply.github.com>
2022-09-08 09:31:08 +0800

    State-Driven Cameras
    Cinemachine

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D myRigidbody2D;
     Animator animator;
     CapsuleCollider2D capsuleCollider2D;
+    float originalGravity;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
         myRigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+
+        originalGravity = myRigidbody2D.gravityScale;
     }
 
     // Update is called once per frame
@@ -32,10 +35,18 @@ public class PlayerMovement : MonoBehaviour
 
     private void ClimbLadder()
     {
-        if (!capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) { return;}
+        if (!capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+            myRigidbody2D.gravityScale = originalGravity; 
+            animator.SetBool("isClimbing", false);
+            return;
+        }
 
         Vector2 climbVelocity = new Vector2(myRigidbody2D.velocity.x, moveInput.y * climbSpeed);
         myRigidbody2D.velocity = climbVelocity;
+        myRigidbody2D.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody2D.velocity.y) > Mathf.Epsilon;
+        animator.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 
     private void FlipSprite()
@@ -54,7 +65,9 @@ public class PlayerMovement : MonoBehaviour
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
         if (playerHasHorizontalSpeed) {
             animator.SetBool("isRunning", true);
-        } 
+        } else {
+            animator.SetBool("isRunning", false);
+        }
     }
 
     void OnMove(InputValue value)
